9730e36bf9ec48b18da4c026b91c94d2d5416807
Megan Xu <69741404+Rainie07@users.noreply.github.com>
2020-10-29 10:44:41 -0600

    finish order

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,23 +1,23 @@
 import { CREATE_ORDER, CLEAR_CART, CLEAR_ORDER, FETCH_ORDERS } from "../types";
 
 export const createOrder = (order) => (dispatch) => {
-  fetch("/api/orders", {
+  fetch("/api/orders", { //send to the server to create an order
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: {"Content-Type": "application/json",},
     body: JSON.stringify(order),
   })
-    .then((res) => res.json())
+    .then((res) => res.json()) //convert the result to json
     .then((data) => {
-      dispatch({ type: CREATE_ORDER, payload: data });
+      dispatch({ type: CREATE_ORDER, payload: data });//the data is order data
       localStorage.clear("cartItems");
       dispatch({ type: CLEAR_CART });
     });
 };
+
 export const clearOrder = () => (dispatch) => {
   dispatch({ type: CLEAR_ORDER });
 };
+
 export const fetchOrders = () => (dispatch) => {
   fetch("/api/orders")
     .then((res) => res.json())
